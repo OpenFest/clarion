78c73d273ed9bbc3cbeea747fc8b4295db46853f
Petko Bordjukov <bordjukov@gmail.com>
2024-04-18 21:48:34 +0300

    Make FROM mail addresses more friendly

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -13,7 +13,7 @@ class VolunteerMailer < ActionMailer::Base
     I18n.locale = @volunteer.language
     mail(to: @volunteer.email,
          reply_to: @volunteer.conference.email,
-         from: "cfp@openfest.org",
+         from: "OpenFest <cfp@openfest.org>",
          subject: I18n.t("volunteer_mailer.success_notification.subject",
            conference_name: @volunteer.conference.title))
   end
@@ -23,7 +23,7 @@ class VolunteerMailer < ActionMailer::Base
     I18n.locale = @volunteer.language
     mail(to: @volunteer.email,
          reply_to: @volunteer.conference.email,
-         from: "cfp@openfest.org",
+         from: "OpenFest <cfp@openfest.org>",
          subject: I18n.t("volunteer_mailer.email_confirmation.subject",
                          conference_name: @volunteer.conference.title))
   end
