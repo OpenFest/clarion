2d5140ba44a01435b73acdf233bbf1f42182427a
Albert Stefanov <aastefanov@outlook.com>
2025-06-08 07:03:21 +0000

    Revert "Fix volunteer counts shown for a team (#46)"
    This reverts commit bcac28d4ff47061d8440c075497b75302020cedb.
    
    This breaks the whole management interface...

--- app/models/volunteer_team.rb
+++ app/models/volunteer_team.rb
@@ -1,7 +1,6 @@
 class VolunteerTeam < ActiveRecord::Base
   belongs_to :conference
-  has_many :volunteers, inverse_of: :volunteer_team
-  has_and_belongs_to_many :supporters, class_name: "Volunteer", inverse_of: :additional_volunteer_teams
+  has_and_belongs_to_many :volunteers
 
   validates :name, presence: true
   validates :color, presence: true, format: {with: /\A#?[a-f0-9]{6}\z/i}
